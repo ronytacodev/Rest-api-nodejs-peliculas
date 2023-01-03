25f36260155699bf6e55d57d95c5539b274f1355
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-01-03 10:32:57 -0500

    usando nodemon

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
 // middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+// routes
+app.get('/', (req,res) => {
+    res.json({"Title": "hello world"});
+});
+
 // starting the server
-app.listen(3000, () => {
-    console.log(`Server on port ${3000}`);
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
