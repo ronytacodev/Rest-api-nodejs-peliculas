ed2a576daf290c028523f89ea1563baa6dfeeea6
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-01-04 02:47:06 -0500

    agregando delete y put a la api

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
 const router = Router();
+const _ = require('underscore');
 
 const movies = require('../sample.json');
 console.log(movies);
@@ -19,9 +20,37 @@ router.post('/', (req, res) => {
         res.json(movies);
     } else {
         // res.send('Wrong Request');
-        res.json({error: 'There was an error'});
-        // me quede en el min 54.23
+        res.status(500).json({error: 'There was an error'});
     }
+});
+
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const {title, director, year, rating} = req.body;
+    if (title && director && year && rating) {
+        _.each(movies, (movie, i) => {
+            if (movie.id == id) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.json(movies);
+    } else {
+        res.status(500).json({error: 'There was an error'});
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    const {id}= req.params;
+    _.each(movies, (movie, i ) => {
+        if (movie.id == id) {
+            movies.splice(i , 1);
+        }
+    });
+    // console.log(req.params);
+    res.send(movies);
 });
 
 module.exports = router;
